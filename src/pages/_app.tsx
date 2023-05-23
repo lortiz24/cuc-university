d4ae9b02cfa77b40e80c416e227f63541d17d4f2
lortiz24 <luis.ortiz@evius.co>
2023-05-23 16:00:54 -0500

    nuevo repo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import { ThemeProvider, CssBaseline } from '@mui/material'
 import type { AppProps } from 'next/app'
 
 
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={lightTheme}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 import { MainLayout } from '@/components/layouts/MainLayout'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      <MainLayout title='Cuc University' >
+  <MainLayout title='Cuc University'>
 
-      </MainLayout>
-    </>
+  </MainLayout>
   )
 }
