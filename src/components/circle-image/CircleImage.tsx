83b1540f95387c6913467f61a8bed79779f8aa14
lortiz24 <luis.ortiz@evius.co>
2023-05-24 13:33:27 -0500

    nabvar dos

--- src/components/circle-image/CircleImage.tsx
+++ src/components/circle-image/CircleImage.tsx
@@ -1,6 +1,6 @@
-import { useTheme } from '@mui/material';
+import { useTheme, Typography, Grid } from '@mui/material';
 import Image from 'next/image';
-
+import MyGradiantBackground from './ColorBackground';
 
 
 interface Props {
@@ -11,7 +11,9 @@ export const CircleImage = ({ src }: Props) => {
     const theme = useTheme()
 
     return (
-        <div style={{ position: 'relative', width: '100%' }}>
+        <div
+            style={{ position: 'relative', width: '100%', height: 400 }}
+        >
             <Image
                 src={src}
                 alt="Imagen"
@@ -19,24 +21,29 @@ export const CircleImage = ({ src }: Props) => {
                 height={0}
                 sizes="100vw"
                 style={{
+                    position: "absolute",
                     width: '100%',
                     height: '400px',
                     borderRadius: '40rem',
                     objectFit: 'cover',
-                    objectPosition: 'right top'
+                    objectPosition: 'right top',
+                    zIndex: -200
                 }} />
-            <div
-                style={{
-                    borderRadius: '40rem',
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    width: '100%',
-                    height: '100%',
-                    backgroundColor: theme.palette.secondary.main,
-                    opacity: 0.8,
-                }}
-            ></div>
+            <MyGradiantBackground color={theme.palette.secondary.main} borderRadius='40rem' />
+
+            <Grid
+            padding={{xs:2,md:8}}
+                container
+                spacing={2}
+                >
+                <Grid item xs={6} md={8}>
+                    
+                </Grid>
+                <Grid item xs={6} md={4}>
+                    <Typography>xs=6 md=4</Typography>
+                </Grid>
+                
+            </Grid>
         </div>
 
 
