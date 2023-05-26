7b41bd4010b2c4d07eefefc29275e2055c0c46e7
Andres Barros <abarros17@cuc.edu.co>
2023-05-26 00:52:50 -0500

    navbar escritorio listo

--- src/components/ui/Navbar2.tsx
+++ src/components/ui/Navbar2.tsx
@@ -1,27 +1,21 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import { useTheme } from '@mui/material';
-import { ActiveLink } from '../active-link/ActiveLink';
 import { useRouter } from 'next/router';
 import styles from './Navbar.module.css';
+import Popover from '@mui/material/Popover';
+import MenuItem from '@mui/material/MenuItem';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { ActiveLink } from '../active-link/ActiveLink';
 
 interface Props {
   window?: () => Window;
 }
 
-const drawerWidth = 240;
+const origin = (typeof window === 'undefined') ? '' : window.location.origin;
 
 export interface MenuItem {
   path: string;
@@ -32,118 +26,58 @@ export interface MenuItem {
 const navItems: MenuItem[] = [
   {
     path: '/',
-    name: 'Home'
+    name: 'Home',
   },
   {
     name: 'About Us',
-    path: '/about-us'
+    path: '/about-us',
   },
   {
     name: 'Academic Programs',
     path: '/academic-programs',
     dropdown: [
       {
-        path: '/academic-program-1',
-        name: 'Academic Program 1'
+        name: 'Undergraduate Programs',
+        path: '/program-1',
       },
       {
-        path: '/academic-program-2',
-        name: 'Academic Program 2'
-      }
-    ]
+        name: 'Graduate Programs',
+        path: '/program-2',
+      },
+    ],
   },
   {
     name: 'Students',
     path: '/students',
-    dropdown: [
-      {
-        path: '/student-1',
-        name: 'Student 1'
-      },
-      {
-        path: '/student-2',
-        name: 'Student 2'
-      },
-      {
-        path: '/student-3',
-        name: 'Student 3'
-      }
-    ]
   },
   {
     name: 'Scholarships',
-    path: '/scholarships'
+    path: '/scholarships',
   },
   {
     name: 'Contact Us',
-    path: '/contact-us'
-  }
+    path: '/contact-us',
+  },
 ];
-
-const origin = (typeof window === 'undefined') ? '' : window.location.origin;
 
 export const NavbarUi = (props: Props) => {
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
-  const [expandedMenus, setExpandedMenus] = React.useState<string[]>([]);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const router = useRouter();
+
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
   };
-  const router = useRouter();
 
-  const toggleMenu = (path: string) => {
-    if (expandedMenus.includes(path)) {
-      setExpandedMenus((prevMenus) => prevMenus.filter((menu) => menu !== path));
-    } else {
-      setExpandedMenus((prevMenus) => [...prevMenus, path]);
-    }
+  const handleMenuOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
   };
 
-  const isMenuExpanded = (path: string) => {
-    return expandedMenus.includes(path);
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    console.log('hola')
   };
-
-  const renderSubMenu = (menuItems: MenuItem[]) => {
-    return (
-      <List disablePadding>
-        {menuItems.map((menuItem) => (
-          <ListItem key={menuItem.name} disablePadding>
-            <ListItemButton sx={{ textAlign: 'center' }} onClick={() => router.push(menuItem.path)}>
-              <ActiveLink text={menuItem.name} href={menuItem.path} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    );
-  };
-
-  
-
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        Cuc University
-      </Typography>
-      <Divider />
-      <List>
-        {navItems.map((menuItem) => (
-          <ListItem key={menuItem.name} disablePadding>
-            <ListItemButton sx={{ textAlign: 'center' }} onClick={() => toggleMenu(menuItem.path)}>
-              {
-                  menuItem.path !== '/academic-programs' ?
-            <ActiveLink text={menuItem.name} href={menuItem.path} />
-            :
-            <a >{menuItem.name}</a>
-
-
-              }
-            </ListItemButton>
-            {isMenuExpanded(menuItem.path) && menuItem.dropdown && renderSubMenu(menuItem.dropdown)}
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
 
   const container = window !== undefined ? () => window().document.body : undefined;
 
@@ -156,14 +90,15 @@ export const NavbarUi = (props: Props) => {
           marginBottom: { xs: 4, md: 4 },
           paddingX: { xs: 4, md: 10 },
         }}
-        component={'nav'}
+        component='nav'
         elevation={0}
-        color='transparent'>
+        color='transparent'
+      >
         <Toolbar>
           <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
+            color='inherit'
+            aria-label='open drawer'
+            edge='start'
             onClick={handleDrawerToggle}
             sx={{ mr: 2, display: { lg: 'none' } }}
           >
@@ -171,39 +106,60 @@ export const NavbarUi = (props: Props) => {
           </IconButton>
           <div style={{ flexGrow: 1 }}>
             <Box
-              component={'img'}
+              component='img'
               sx={{
                 maxHeight: { xs: 200, md: 167 },
                 maxWidth: { xs: 300, md: 250 },
-                display: { xs: 'none', lg: 'block' }
+                display: { xs: 'none', lg: 'block' },
               }}
-              alt="The house from the offer."
-              src={`${origin}/assets/9744a3a642b478781df6cd9b3dde8724.png`} />
+              alt='The house from the offer.'
+              src={`${origin}/assets/9744a3a642b478781df6cd9b3dde8724.png`}
+            />
           </div>
-
-          <Box sx={{ display: { xs: 'none', lg: 'block' } }}>
-            <nav className={styles['menu-container']} >
+          <Box sx={{ display: { xs: 'none', lg: 'block' } }}   onMouseLeave={handleMenuClose}>
+            <nav className={'menu-container'}>
               {navItems.map((menuItem) => (
                 <React.Fragment key={menuItem.name}>
                   {menuItem.dropdown ? (
-                    <div className="dropdown-container">
-                      <button
-                        className={`dropdown-button ${isMenuExpanded(menuItem.path) ? 'active' : ''}`}
-                        onClick={() => toggleMenu(menuItem.path)}
-                      >
+                    <div
+                      onMouseEnter={handleMenuOpen}
+                      style={{ position: 'relative' }}
+                    >
+                      <button className={styles['nav-item-button']}>
                         {menuItem.name}
+                        <ExpandMoreIcon className={styles['expand-icon']} />
                       </button>
-                      {isMenuExpanded(menuItem.path) && menuItem.dropdown && (
-                        <div className="dropdown-menu">
-                          {renderSubMenu(menuItem.dropdown)}
-                        </div>
-                      )}
+                      <Popover
+                        open={Boolean(anchorEl)}
+                        anchorEl={anchorEl}
+                        onClose={handleMenuClose}
+                        anchorOrigin={{
+                          vertical: 'bottom',
+                          horizontal: 'left',
+                        }}
+                        transformOrigin={{
+                          vertical: 'top',
+                          horizontal: 'left',
+                        }}
+                      >
+                        <Box sx={{ minWidth: '200px' }}>
+                          {menuItem.dropdown.map((subItem) => (
+                            <MenuItem
+                              key={subItem.name}
+                              onClick={() => {
+                                handleMenuClose();
+                                router.push(subItem.path);
+                              }}
+                              sx={{ width: '100%' }}
+                            >
+                              {subItem.name}
+                            </MenuItem>
+                          ))}
+                        </Box>
+                      </Popover>
                     </div>
                   ) : (
-                    <ActiveLink
-                      text={menuItem.name}
-                      href={menuItem.path}
-                    />
+                    <ActiveLink text={menuItem.name} href={menuItem.path} />
                   )}
                 </React.Fragment>
               ))}
@@ -211,23 +167,6 @@ export const NavbarUi = (props: Props) => {
           </Box>
         </Toolbar>
       </AppBar>
-      <Box component="nav">
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block', lg: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-        >
-          {drawer}
-        </Drawer>
-      </Box>
     </>
   );
-}
+};
