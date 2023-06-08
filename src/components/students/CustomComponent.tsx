0ed8ae8972f828f972f1445f15f0a8e13d80d518
lortiz24 <luis.ortiz@evius.co>
2023-06-07 22:55:16 -0500

    min hegihh en students

--- src/components/students/CustomComponent.tsx
+++ src/components/students/CustomComponent.tsx
@@ -8,31 +8,45 @@ interface CustomComponentProps {
 
 const CustomComponent: React.FC<CustomComponentProps> = ({ title, items }) => {
   return (
-<Grid item xs={12} md={6} marginY={5}>
-    <Typography
+    <Grid
+      item
+      xs={12}
+      md={6}
+      marginY={5}>
+      <Typography
         variant="h4"
         color="primary"
         fontWeight="bold"
         align='center'
         paddingBottom={1}
-        >
+      >
         {title}
       </Typography>
-      <Box sx={{ border: 1, borderColor: '#E0CA83', borderRadius: '40px', py: 2 , px: 1 , minHeight:'447px' }}>
+      <Box sx={{
+        border: 1,
+        borderColor: '#E0CA83',
+        borderRadius: '40px',
+        py: 2,
+        px: 1,
+        minHeight: { sm: '0px', md: '950px', lg: '550px' },
+        '@media (min-width: 1900px)': {
+          minHeight:'460px'
+        },
+      }}>
         <List disablePadding>
           {items.map((item, index) => (
             <ListItem key={index} sx={{ py: 0.3 }}>
               <ListItemText
                 primary={
                   <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                    <Typography variant="body1" fontWeight="bold" fontSize={{xs:15,sm:20}}>
+                    <Typography variant="body1" fontWeight="bold" fontSize={{ xs: 15, sm: 20 }}>
                       {item.date + ' '}
                     </Typography>
-                    <Typography variant="body1" component="span" sx={{ marginLeft: '0.5rem' ,marginRight:'0.5rem'}}>
+                    <Typography variant="body1" component="span" sx={{ marginLeft: '0.5rem', marginRight: '0.5rem' }}>
                       {' - '}
                     </Typography>
-                    <Typography variant="body1" component="span" fontSize={{xs:15,sm:20}}>
-                      { ' ' + item.description}
+                    <Typography variant="body1" component="span" fontSize={{ xs: 15, sm: 20 }}>
+                      {' ' + item.description}
                     </Typography>
                   </Box>
                 }
